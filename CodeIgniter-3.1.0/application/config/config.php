40c968c3fcc6c62f780482af19e1130de680aeab
Xiaumay <gx.apple@gmail.com>
2016-12-05 13:52:54 +0100

    Mappen aangepast

--- CodeIgniter-3.1.0/application/config/config.php
+++ CodeIgniter-3.1.0/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/pev/';
+$config['base_url'] = 'http://xiaumayeysink.nl/PEV/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/pev/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
